1373cb0a6e36de61da4c500330fd3f1fbfd6fed8
Alys Brooks <alysbrooks@fastmail.com>
2025-08-09 14:42:12 -0500

    Initial working version.
    Focus now is aggregating at a high level.

---
+++ repl_sessions/core.clj
@@ -0,0 +1,46 @@
+(ns core
+  (:require [clojure-stats.core :refer :all] )
+  (:import [java.io File]))
+
+(read-file "../shape-of-clojure/test/clojure/clojurescript/src/main/clojure/cljs/cli.clj")
+
+(enumerate-symbols (read-file "../shape-of-clojure/test/clojure/clojurescript/src/main/clojure/cljs/cli.clj"))
+(classify-symbols (read-file "../shape-of-clojure/test/clojure/clojurescript/src/main/clojure/cljs/cli.clj"))
+
+
+(-> (read-file "../shape-of-clojure/test/clojure/clojurescript/src/main/clojure/cljs/cli.clj")
+    first 
+    meta)
+
+(-> (read-file "../shape-of-clojure/test/clojure/clojurescript/src/main/clojure/cljs/cli.clj")
+    first 
+    first
+    resolve)
+
+
+(let [clj-files (->> (file-seq (File. "."))
+                     (filter #(.endsWith (.getName %) ".clj"))) 
+      counts (for [file clj-files]
+               (classify-symbols (read-file file)))]
+
+  (apply merge-with + counts))
+
+(let [clj-files (->> (file-seq (File. "../shape-of-clojure/test/clojure/clojurescript/src"))
+                     (filter #(.endsWith (.getName %) ".clj"))) 
+      
+      counts (for [file clj-files]
+               (classify-symbols (read-file file))) ]
+
+  (apply merge-with + counts))
+
+(let [clj-files (->> (file-seq (File. "../shape-of-clojure/test/technomancy/leiningen/src"))
+                     (filter #(.endsWith (.getName %) ".clj"))) 
+      
+      counts (for [file clj-files]
+               (classify-symbols (read-file file))) ]
+
+  (apply merge-with + counts))
+
+(classify-files "../shape-of-clojure/test/technomancy/leiningen/src")
+
+*e
---
+++ src/clojure_stats/core.clj
@@ -0,0 +1,98 @@
+(ns clojure-stats.core
+  (:require [edamame.core :as e]
+            [clojure.walk :as walk]
+            [clojure.tools.cli])
+  (:import [java.io File])
+  )
+
+
+(def clojure-defaults {:all true
+                       :row-key :line
+                       :col-key :column
+                       :end-location false
+                       :location? seq?})
+
+(defn read-file 
+  "Reads a file"
+  [file]
+  (-> (slurp file)
+      (e/parse-string-all (merge clojure-defaults {:auto-resolve-ns true :fn true}) )))
+
+(defn try-resolve [symbol extra-aliases]
+
+  (let [resolved (resolve symbol) ]
+    (or resolved symbol)))
+
+(defn enumerate-symbols
+  "Enumerates symbols inside a form"
+  [forms]
+
+  (->> (for [form forms] 
+        (->> (tree-seq seq? identity form)
+             (filter symbol?)
+             (map #( try-resolve % nil))
+             frequencies))
+      (reduce #(merge-with + %1 %2))))
+
+(defn classify-symbol
+  "Classifies a symbol broadly"
+  [sym]
+  (let [resolved (try-resolve sym nil)
+        resolved (if (instance? java.lang.Class resolved) (symbol (.getName resolved)) resolved)
+
+        ]
+    (try 
+      (let [resolved-ns (namespace (symbol resolved))]
+        (cond 
+        (special-symbol? sym) :special
+        (= resolved-ns "clojure.core") :ns/clojure.core
+        (not (nil? resolved-ns)) (keyword "ns" resolved-ns )
+        :else :other))
+
+      (catch IllegalArgumentException e 
+        (prn (type resolved) resolved)
+        :error))))
+
+(defn classify-symbols [forms]
+  (->> (for [form forms] 
+        (->> (tree-seq seq? identity form)
+
+             (filter symbol?)
+             (map classify-symbol)
+             frequencies))
+
+      (reduce #(merge-with + %1 %2))))
+
+(defn classify-files [path]
+  (let [clj-files (->> (file-seq (File. path))
+                       (filter #(.endsWith (.getName %) ".clj"))) 
+
+        counts (->> (for [file clj-files]
+                     (try 
+                       [(str file)
+                        (classify-symbols (read-file file))]
+                       (catch Exception e
+                         (prn #_e (ex-data e) (ex-cause e))
+                         [file nil])))
+                   (into {}))]
+    counts))
+
+
+;; (apply merge-with + counts)
+
+(defn aggregate-top-level [results]
+  (apply merge-with + results))
+
+
+(def cli-options 
+  [["-a" "--analysis" :parse-fn keyword]
+   ["-h" "--help"]])
+
+(defn -main [& args]
+
+  (let [{:keys [arguments] {:keys [analysis]} :options :as parsed} (clojure.tools.cli/parse-opts args cli-options)]
+    (prn parsed)
+    (for [arg arguments]
+      (classify-files arg))))
+
+
